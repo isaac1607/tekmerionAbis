f96bd1239fccdf91e638238102a003a66b4a7a70
USER <coulisaac99@gmail.com>
2025-04-28 21:49:52 +0000

    First commit

---
+++ rest/client/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from abis.rest.client.api.applicant_api import ApplicantApi
+from abis.rest.client.api.configuration_api import ConfigurationApi
+from abis.rest.client.api.data_exchange_api import DataExchangeApi
+from abis.rest.client.api.hitlist_api import HitlistApi
+from abis.rest.client.api.matching_result_api import MatchingResultApi
+from abis.rest.client.api.onboarding_api import OnboardingApi
+from abis.rest.client.api.rpc_api import RPCApi
+from abis.rest.client.api.request_api import RequestApi
+
